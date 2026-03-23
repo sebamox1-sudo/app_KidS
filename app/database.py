4060a18b5d602c423bfe881c04df3d20bef845af
sebamox1-sudo <seba.mox1@gmail.com>
2026-03-23 12:59:42 +0100

    fix database URL override

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(override=False)
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/kids_db")
 
